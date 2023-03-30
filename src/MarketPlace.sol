ac70428631b5a4aa15477d3ad613d62b3bb45ca7
g <5714678+giuseppecrj@users.noreply.github.com>
2023-03-30 11:27:17 -0700

    add reqs

---
+++ src/MarketPlace.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.19;
+
+contract MarketPlace {
+// Requirements:
+// As a seller, I want to list an NFT for sale
+// As a seller, I want to cancel an NFT from sale
+// As a seller, I want to withdraw the funds from a sale
+// As a buyer, I want to buy an NFT
+}
