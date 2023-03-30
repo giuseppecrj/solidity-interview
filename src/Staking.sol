d15b3dd0022d9835462a28ec4aaa061a24bd72b0
g <5714678+giuseppecrj@users.noreply.github.com>
2023-03-30 11:41:45 -0700

    add staking challenge

---
+++ src/Staking.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+// interfaces
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// libraries
+
+// contracts
+
+contract SimpleStaking {
+// Requirements:
+// The staking contract should be able to receive ERC20 tokens
+// The contract should keep track of the total amount of tokens staked
+// The contract should keep track of a reward rate
+// The contract should have a stake function that takes in an amount of tokens to stake
+// The contract should have a withdraw function that takes in an amount of tokens to withdraw
+// The contract should have a claim function that calculates the reward for the user and sends it to the user
+// The contract should have a getReward function that calcuale the reward for the user based on (amount of tokens staked * reward rate * time since last claim) / 1e18
+}
